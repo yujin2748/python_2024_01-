3131a85e106fe631797d0c1021612d987c90c2cc
yujin2748 <155503281+yujin2748@users.noreply.github.com>
2024-01-03 12:50:21 +0900

    Add files via upload

---
+++ Day01/Quiz.py
@@ -0,0 +1,15 @@
+# 자기소개
+# 이름 입력:
+# mbti 입력:
+# 좋아하는 음식 입력:
+# 결과 => 이름이 ~~이시고, mbti가 ~~이시고, 좋아하는 음식이 ~~이군요
+
+
+name = input("당신의 이름은?:")
+mbti = input("mbti는?:")
+food = input("좋아하는 음식은?:")
+print(f"이름은 {name}이시고, mbti가 {mbti}이시고, 좋아하는 음식이 {food}이군요😊")
+
+movie = input("최근에 본 영화:")
+ott = input("최근에 본 ott:")
+print(f"최근에 본 영화는 {movie} 최근에 본 ott는 {ott}")
---
+++ Day01/output_input.py
@@ -0,0 +1,19 @@
+# 메모용 / 주석문 [코드 실행안됨]
+# print('이름 : 오유진  배우는 이유 : 취미')
+
+# f문자열문법: print(f"") 연산이 들어가는 경우 {} 사용하기
+print(f"123 + 456 + 789 = {123 + 456 + 789}")
+
+# 변수: 값을 담는 상자
+
+#name변수가 사용됨
+# = (넣는다)
+name = "오유진"
+mbti = "istp"
+print(name)
+print(mbti)
+print(f"저의 이름은 {name} mbti는 {mbti}")
+
+#input() [유저한테 입력값을 받는 기능]
+your_name = input("당신의 이름은:")
+print(f"당신의 이름은 {your_name}이군요!😊")
