243356a758da6ca08053a1ae8461ab7ae8966324
yujin2748 <155503281+yujin2748@users.noreply.github.com>
2024-01-04 12:52:18 +0900

    Add files via upload

---
+++ Day02/data type.py
@@ -0,0 +1,20 @@
+'''
+first = int(input("첫 번째 수:"))
+second = int(input("두 번째 수:"))
+
+print(f"합: {first + second}")
+print(f"차: {first - second}")
+print(f"곱: {first * second}")
+'''
+
+# input() : 유저한테 입력받는 기능
+# print() : 안에 들어있는 내용 출력
+# int() : 안에 들어있는 문자를 정수화
+
+a = 10 # a는 숫자 정수형 타입
+b = 3.14 # b는 숫자 실수형 타입
+c = "megastudy" #c는 문자형 타입
+
+# cpu[연산] + ram[기억]
+a = 10 # (=)대입 연산자
+b = 3.14
---
+++ Day02/review.py
@@ -0,0 +1,9 @@
+time = input("오늘 아침 일어난 시간은?")
+food = input("오늘 아침 먹은 메뉴는?")
+
+# 연산(operator)
+# 1+2+3 : 사칙 연산
+# time : 호출 연산
+# and or not : 논리 연산
+
+print(f"오늘 기상 시간은 {time}이고, 먹은 메뉴는 {food}군요!")
